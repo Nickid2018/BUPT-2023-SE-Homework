f9a496e51bf2c64ec8e2b1a0e276d7c04e558348
Nickid2018 <nickid2018@outlook.com>
2023-09-17 22:06:14 +0800

    update prettier configuration

--- frontend/prettier.config.js
+++ frontend/prettier.config.js
@@ -1,19 +1,17 @@
 // prettier.config.js
 module.exports = {
-  plugins: ["prettier-plugin-tailwindcss"],
-  tailwindConfig: "./tailwind.config.js",
-  arrowParens: "avoid",
-  bracketSpacing: true,
-  endOfLine: "crlf",
-  semi: true,
-  printWidth: 120,
-  trailingComma: "none",
-  overrides: [
-    {
-      files: "*.css",
-      options: {
-        tabWidth: 4,
-      },
-    },
-  ],
+    arrowParens: "avoid",
+    bracketSpacing: true,
+    endOfLine: "crlf",
+    semi: true,
+    printWidth: 120,
+    trailingComma: "none",
+    overrides: [
+        {
+            files: ["*.css", "*.ts"],
+            options: {
+                tabWidth: 4,
+            }
+        }
+    ]
 };
