1be0c434d5af8433dc674073d69083e5e22226ed
sugle <sugle_w@foxmail.com>
2023-11-27 11:51:19 +0800

    tool

---
+++ backend/app/utils.py
@@ -0,0 +1,9 @@
+from flask import request
+
+
+# 需要修改
+def check_csrf_token(request):
+    csrf_token = request.headers.get('X-CSRF-Token')
+
+    # 在这里进行比较
+    return csrf_token == 'your_expected_csrf_token'   # 这里只是示例
