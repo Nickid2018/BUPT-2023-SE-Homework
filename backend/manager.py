b998ae4d53c81fbea87ab17a658009d28f0897ce
sugle <sugle_w@foxmail.com>
2023-11-27 11:50:44 +0800

    修改 modify

--- backend/manager.py
+++ backend/manager.py
@@ -1,4 +1,10 @@
-from app.app import app
+from app import create_app, db
+from flask_script import Manager
+from flask_migrate import Migrate
+
+app = create_app()
+migrate = Migrate(app, db)
+
 
 if __name__ == '__main__':
     app.run()
