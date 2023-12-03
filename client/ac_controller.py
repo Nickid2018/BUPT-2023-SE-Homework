829e6ce1a7aaa1e3c3db4d45f21de1f775b8b5e3
distec1 <distec001@icloud.com>
2023-12-03 12:00:40 +0800

    reformat successfully

--- client/ac_controller.py
+++ client/ac_controller.py
@@ -3,90 +3,92 @@ from PyQt5.QtCore import QTimer, QObject
 from network_manager import NetworkManager
 from database_manager import DatabaseManager
 
+
 class ACController(QObject):
     def __init__(self, network_manager, database_manager):
         super().__init__()
         self.network_manager = network_manager
         self.database_manager = database_manager
         self.current_state = {
-            'room_id': '228',
-            'power': False,  # 初始状态为关
-            'set_temperature': 28,  # 用户设定的初始温度
-            'current_temperature': 28,  # 模拟的当前房间温度
-            'mode': '制冷',  # 初始模式
-            'wind_speed': '中',  # 初始风速
-            'sweep': False  # 初始扫风状态
+            "room_id": "228",
+            "power": False,  # 初始状态为关
+            "set_temperature": 28,  # 用户设定的初始温度
+            "current_temperature": 28,  # 模拟的当前房间温度
+            "mode": "制冷",  # 初始模式
+            "wind_speed": "中",  # 初始风速
+            "sweep": False,  # 初始扫风状态
         }
         self.temp_change_timer = QTimer()
         self.temp_change_timer.timeout.connect(self.update_temperature)
         self.temp_change_timer.start(10000)  # 每10秒更新一次温度
 
     def update_temperature(self):
-        """ 定时更新房间温度 """
-        if self.current_state['power']:
-            self.current_state['current_temperature'] += 0.5
+        """定时更新房间温度"""
+        if self.current_state["power"]:
+            self.current_state["current_temperature"] += 0.5
             self.log_state()
 
     def toggle_power(self):
-        """ 切换空调的开关状态 """
-        self.current_state['power'] = not self.current_state['power']
+        """切换空调的开关状态"""
+        self.current_state["power"] = not self.current_state["power"]
         self.log_state()
 
     def increase_temperature(self):
-        """ 提高空调设定温度 """
-        if self.current_state['set_temperature'] < 30:
-            self.current_state['set_temperature'] += 1
+        """提高空调设定温度"""
+        if self.current_state["set_temperature"] < 30:
+            self.current_state["set_temperature"] += 1
         self.log_state()
 
     def decrease_temperature(self):
-        """ 降低空调设定温度 """
-        if self.current_state['set_temperature'] > 18:
-            self.current_state['set_temperature'] -= 1
+        """降低空调设定温度"""
+        if self.current_state["set_temperature"] > 18:
+            self.current_state["set_temperature"] -= 1
         self.log_state()
 
     def toggle_mode(self):
-        """ 切换空调模式（制热/制冷）"""
-        self.current_state['mode'] = '制热' if self.current_state['mode'] == '制冷' else '制冷'
+        """切换空调模式（制热/制冷）"""
+        self.current_state["mode"] = (
+            "制热" if self.current_state["mode"] == "制冷" else "制冷"
+        )
         self.log_state()
 
     def change_wind_speed(self):
-        """ 调节风速（高/中/低）"""
-        speeds = ['高', '中', '低']
-        index = speeds.index(self.current_state['wind_speed'])
-        self.current_state['wind_speed'] = speeds[(index + 1) % len(speeds)]
+        """调节风速（高/中/低）"""
+        speeds = ["高", "中", "低"]
+        index = speeds.index(self.current_state["wind_speed"])
+        self.current_state["wind_speed"] = speeds[(index + 1) % len(speeds)]
         self.log_state()
 
     def toggle_sweep(self):
-        """ 切换扫风状态 """
-        self.current_state['sweep'] = not self.current_state['sweep']
+        """切换扫风状态"""
+        self.current_state["sweep"] = not self.current_state["sweep"]
         self.log_state()
 
     def log_state(self):
-        """ 记录当前状态到数据库 """
-        self.current_state['time'] = time.strftime('%Y-%m-%dT%H:%M:%S')
+        """记录当前状态到数据库"""
+        self.current_state["time"] = time.strftime("%Y-%m-%dT%H:%M:%S")
         self.database_manager.log_state(self.current_state)
 
         # 更新 GUI 显示
-        if hasattr(self, 'update_callback'):
+        if hasattr(self, "update_callback"):
             self.update_callback()
 
     def set_update_callback(self, callback):
-        """ 设置一个回调函数用于更新 GUI 状态 """
+        """设置一个回调函数用于更新 GUI 状态"""
         self.update_callback = callback
 
     def get_current_state(self):
-        """ 返回当前的状态 """
+        """返回当前的状态"""
         return self.current_state
-
 
     def send_update(self, operation, data):
         # 构建请求数据
         request_data = {
-            'operation': operation,
-            'data': data,
-            'time': time.strftime('%Y-%m-%dT%H:%M:%S+08:00'),
-            'unique_id': self.network_manager.generate_unique_id(),
-            'signature': ''  # 需要实现签名逻辑
+            "operation": operation,
+            "data": data,
+            "time": time.strftime("%Y-%m-%dT%H:%M:%S+08:00"),
+            "unique_id": self.network_manager.generate_unique_id(),
+            "signature": "",  # 需要实现签名逻辑
         }
         self.network_manager.send_request(request_data)
 
--- client/gui.py
+++ client/gui.py
@@ -1,5 +1,14 @@
-from PyQt5.QtWidgets import QMainWindow, QLabel, QPushButton, QVBoxLayout, QHBoxLayout, QWidget, QFrame
+from PyQt5.QtWidgets import (
+    QMainWindow,
+    QLabel,
+    QPushButton,
+    QVBoxLayout,
+    QHBoxLayout,
+    QWidget,
+    QFrame,
+)
 from PyQt5.QtCore import pyqtSlot
+
 
 class MainAppWindow(QMainWindow):
     def __init__(self, ac_controller):
@@ -10,7 +19,7 @@ class MainAppWindow(QMainWindow):
 
     def initUI(self):
         # 设置窗口标题和大小
-        self.setWindowTitle('酒店空调控制系统')
+        self.setWindowTitle("酒店空调控制系统")
         self.setGeometry(300, 300, 400, 400)
 
         # 创建布局和中心小部件
@@ -24,12 +33,12 @@ class MainAppWindow(QMainWindow):
         self.status_layout = QVBoxLayout(self.status_frame)
         main_layout.addWidget(self.status_frame)
         self.status_labels = {
-            'set_temperature': QLabel('设定温度：'),
-            'current_temperature': QLabel('当前房间温度：'),
-            'sweep': QLabel('扫风：'),
-            'wind_speed': QLabel('风速：'),
-            'mode': QLabel('模式：'),
-            'power': QLabel('电源：')
+            "set_temperature": QLabel("设定温度："),
+            "current_temperature": QLabel("当前房间温度："),
+            "sweep": QLabel("扫风："),
+            "wind_speed": QLabel("风速："),
+            "mode": QLabel("模式："),
+            "power": QLabel("电源："),
         }
         for label in self.status_labels.values():
             self.status_layout.addWidget(label)
@@ -40,41 +49,43 @@ class MainAppWindow(QMainWindow):
         main_layout.addLayout(control_layout)
 
         # 添加按钮
-        self.power_button = QPushButton('电源开/关', self)
+        self.power_button = QPushButton("电源开/关", self)
         self.power_button.clicked.connect(self.ac_controller.toggle_power)
         control_layout.addWidget(self.power_button)
 
-        self.temp_up_button = QPushButton('温度升高', self)
+        self.temp_up_button = QPushButton("温度升高", self)
         self.temp_up_button.clicked.connect(self.ac_controller.increase_temperature)
         control_layout.addWidget(self.temp_up_button)
 
-        self.temp_down_button = QPushButton('温度降低', self)
+        self.temp_down_button = QPushButton("温度降低", self)
         self.temp_down_button.clicked.connect(self.ac_controller.decrease_temperature)
         control_layout.addWidget(self.temp_down_button)
 
-        self.wind_speed_button = QPushButton('风速调节', self)
+        self.wind_speed_button = QPushButton("风速调节", self)
         self.wind_speed_button.clicked.connect(self.ac_controller.change_wind_speed)
         control_layout.addWidget(self.wind_speed_button)
 
-        self.sweep_button = QPushButton('扫风切换', self)
+        self.sweep_button = QPushButton("扫风切换", self)
         self.sweep_button.clicked.connect(self.ac_controller.toggle_sweep)
         control_layout.addWidget(self.sweep_button)
 
-        self.mode_button = QPushButton('模式切换', self)
+        self.mode_button = QPushButton("模式切换", self)
         self.mode_button.clicked.connect(self.ac_controller.toggle_mode)
         control_layout.addWidget(self.mode_button)
 
     def update_status_display(self):
         # 更新状态显示区域
         state = self.ac_controller.get_current_state()
-        self.status_labels['set_temperature'].setText(f'设定温度：{state["set_temperature"]}°C')
-        self.status_labels['current_temperature'].setText(f'当前房间温度：{state["current_temperature"]}°C')
-        self.status_labels['sweep'].setText(f'扫风：{"开" if state["sweep"] else "关"}')
-        self.status_labels['wind_speed'].setText(f'风速：{state["wind_speed"]}')
-        self.status_labels['mode'].setText(f'模式：{state["mode"]}')
-        self.status_labels['power'].setText(f'电源：{"开" if state["power"] else "关"}')
-
-
+        self.status_labels["set_temperature"].setText(
+            f'设定温度：{state["set_temperature"]}°C'
+        )
+        self.status_labels["current_temperature"].setText(
+            f'当前房间温度：{state["current_temperature"]}°C'
+        )
+        self.status_labels["sweep"].setText(f'扫风：{"开" if state["sweep"] else "关"}')
+        self.status_labels["wind_speed"].setText(f'风速：{state["wind_speed"]}')
+        self.status_labels["mode"].setText(f'模式：{state["mode"]}')
+        self.status_labels["power"].setText(f'电源：{"开" if state["power"] else "关"}')
 
     # @pyqtSlot()
     # def toggle_power(self):
