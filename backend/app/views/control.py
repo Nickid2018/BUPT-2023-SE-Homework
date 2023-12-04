ba7c02d4b2e6d12f3f89679dc0e9b5be4ba8afb3
sugle <sugle_w@foxmail.com>
2023-12-04 15:57:06 +0800

    Basic version

--- backend/app/views/control.py
+++ backend/app/views/control.py
@@ -2,10 +2,10 @@
 
 from flask import Blueprint, request, jsonify
 
-control_blueprint = Blueprint('control', __name__)
+control_blueprint = Blueprint("control", __name__)
 
 
-@control_blueprint.route('/control', methods=['POST'])
+@control_blueprint.route("/control", methods=["POST"])
 def server_control():
     """
     Server Operations for AC
@@ -14,30 +14,30 @@ def server_control():
 
     # 进行签名验证，确保数据来自合法的客户端
 
-    operation = data.get('operation')
-    data_value = data.get('data')
+    operation = data.get("operation")
+    data_value = data.get("data")
 
     # 根据操作类型执行相应的操作
-    if operation == 'start':
+    if operation == "start":
         # 启动空调的操作逻辑
         pass
-    elif operation == 'stop':
+    elif operation == "stop":
         # 停止空调的操作逻辑
         pass
-    elif operation == 'temperature':
+    elif operation == "temperature":
         # 调整温度的操作逻辑
         pass
-    elif operation == 'wind_speed':
+    elif operation == "wind_speed":
         # 调整风速的操作逻辑
         pass
-    elif operation == 'mode':
+    elif operation == "mode":
         # 调整模式的操作逻辑
         pass
-    elif operation == 'sweep':
+    elif operation == "sweep":
         # 扫风操作逻辑
         pass
     else:
-        return jsonify({'error_code': 400, 'message': 'Invalid operation'}), 400
+        return jsonify({"error_code": 400, "message": "Invalid operation"}), 400
 
     # 操作成功，返回响应
-    return jsonify({'message': 'Operation successfully'}), 204
+    return jsonify({"message": "Operation successfully"}), 204
