8801fa40e6aa55993932ef8731d6a4ddda100f61
sugle <sugle_w@foxmail.com>
2023-12-06 12:35:58 +0800

    Resolve database initialization issues

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,8 +1,9 @@
 # config.py
 import os
 
+
 class Config:
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.abspath('data/Bupt.db')
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.abspath("data/Bupt.db")
     # SECRET_KEY = ""
     SQLALCHEMY_TRACK_MODIFICATIONS = False
