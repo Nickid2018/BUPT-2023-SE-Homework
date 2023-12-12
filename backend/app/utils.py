e47814635cfdaa3ba27c5b2ea9b251c10c380434
sugle <sugle_w@foxmail.com>
2023-12-12 20:28:53 +0800

    update

--- backend/app/utils.py
+++ backend/app/utils.py
@@ -6,8 +6,8 @@ from queue import Queue
 
 
 # 需要修改
-def check_csrf_token(request):
-    csrf_token = request.headers.get("X-CSRF-Token")
+def check_csrf_token(requests):
+    csrf_token = requests.headers.get("X-CSRF-Token")
 
     # 在这里进行比较
     # return csrf_token == "your_expected_csrf_token"  # 这里只是示例
@@ -19,4 +19,3 @@ def dispatch():
     pass
 
 
-# 费用计算
